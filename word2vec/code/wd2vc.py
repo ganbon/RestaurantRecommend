5152bc2112cf9ddf9e568268d0895701e2068b0b
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-07 14:26:06 +0900

    実装予定に変更

--- word2vec/code/wd2vc.py
+++ word2vec/code/wd2vc.py
@@ -5,8 +5,7 @@ from nlptoolsjp.file_system import *
 from nlptoolsjp.norm import *
 
 
-file_list = glob("hotpepper/*.json")
-kind_list = ["形容詞","形容動詞"]
+file_list = glob("tablog_data/*.json")
 word_list = []
 for file in file_list:
     data = file_load(file)
@@ -16,13 +15,6 @@ for file in file_list:
             kind,sentence = morpheme(d,kind=True,nelogd=True)
             # word_list.append([kind[s]["endform"] for s in sentence if kind[s]["speech"] in kind_list])
             word_list.append([kind[s]["endform"] for s in sentence])
-# d = {}
-# for w in word_list:
-#     if w in d.keys():
-#         d[w] += 1
-#     else:
-#         d[w] = 1
-# print(d)
 model = word2vec.Word2Vec(sg=1,sentences=word_list,vector_size=200,min_count=2, window=5, epochs=20)
 model.save("word2vec_model/food_Skip-gram4.model")
                 
