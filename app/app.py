d9677adc239ce8ec002c22e328eacc55e3c144bf
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-27 19:15:06 +0900

    サーバ本体

--- app/app.py
+++ app/app.py
@@ -1,20 +1,41 @@
-from flask import Flask,render_template, request
-from nlptoolsjp.file_system import file_load
-import os
+from flask import Flask, request, make_response,jsonify
+from nlptoolsjp.file_system import *
+from flask_cors import CORS
+from system.search import search_shop
+from system.recommend import recommend
+import pandas as pd
+from global_data import SHOP_DATA
 
-
-app = Flask(__name__,static_folder='./static')
+app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
-def start_site():
-    if os.path.isfile("data/user/user_status.json"): 
-        return render_template("index.html")
-    else:
-        return render_template("first_setting.html")
+@app.route('/search',methods=['GET','POST'])
+def search_result():
+    data = request.get_json()
+    keyword = data['sentence']
+    result = search_shop(keyword,SHOP_DATA)
+    response = [{"url":shop.shop_data["url"],
+                "name":shop.shop_data["店名"],
+                "genre":shop.shop_data["ジャンル"],
+                "review":shop.shop_data["評価"],
+                "place":shop.shop_data["住所"]} for shop in result]
+    return make_response(jsonify(response))
 
-@app.route('/index',methods=["POST"])
-def index():
-    user_data = file_load("data/user/user_status.json")
-    key_word = request.form.get()
-    key_place = request.form.get()  
+@app.route('/book',methods=['GET','POST'])
+def bookmark(methods=['GET','POST']):
+    data = request.get_json()
+    user_data = file_load("data/csv/user.csv")
+    user_data = pd.concat([user_data,pd.DataFrame(data)])
+    file_create(user_data,"data/csv/user.csv")
+
+@app.route('/recommend',methods=['GET','POST'])
+def recommend_result():
+    user_data = file_load("data/csv/user.csv")
+    lunch_data = recommend(user_data,SHOP_DATA,model_path="data/model/lunch_vgae.pth",date="昼")
+    dinner_data = recommend(user_data,SHOP_DATA,model_path="data/model/dinner_vgae.pth",date="夜")
+    return lunch_data,dinner_data
+
+if __name__ == "__main__":
+    app.run(debug=True)
