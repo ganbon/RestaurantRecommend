3a3b1c36a7a6de06292c8b7d5a356c63289696c7
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-27 19:14:21 +0900

    アプリのシステム

---
+++ app/system/dataset.py
@@ -0,0 +1,69 @@
+import torch
+from torch.nn import Embedding
+from nlptoolsjp.file_system import *
+from nlptoolsjp.norm import clean_text
+import numpy as np
+from torch_geometric.utils import train_test_split_edges
+from torch_geometric.data import Data
+import sys
+sys.path.append("..")
+from shop import Shop
+
+
+class ShopGraphDataset:
+    def __init__(self,user_data,shop_data_list,date,vector_size=2400):
+        self.user_name = list(set(user_data.index))
+        self.user_data = user_data
+        self.date = date
+        embeding = Embedding(len(self.user_name),vector_size)
+        self.shop_name = [shop.shop_data["店名"] for shop in shop_data_list] 
+        user_x = embeding(torch.tensor([i for i in range(len(self.user_name))]))
+        shop_x = torch.stack([torch.from_numpy(np.ravel(shop.shop_vector))  for shop in shop_data_list])
+        self.x = torch.cat([user_x,shop_x])
+        self.edge_index = None
+    
+    def index_edge_create(self):
+        shop_list = []
+        edge_attr = []
+        src = []
+        dst = []
+        for u,user in enumerate(self.user_name):
+            user_df = self.user_data[self.user_data.index==user]
+            user_shop = clean_text(user_df["店名"].tolist(),norm_op=False)
+            user_review = user_df[self.date].tolist()
+            for shop,review in zip(user_shop,user_review):
+                try:
+                    dst.append(len(self.user_name)+self.shop_name.index(shop))
+                    src.append(u)
+                    edge_attr.append(review)
+                except:
+                    shop_list.append(shop)
+                    pass
+        edge_list = torch.tensor([src,dst])
+        self.edge_attr = torch.tensor(edge_attr)
+        self.edge_index = edge_list
+    
+    def load_dataset(self):
+        self.index_edge_create()
+        return Data(x=self.x,edge_index = self.edge_index,edge_attr=self.edge_attr)
+
+
+if __name__=="__main__":
+    shop_data = file_load("../shop_data.json")
+    shop_data_list = [Shop(shop_data=shop) for shop in shop_data]
+    for i,s in enumerate(shop_data_list):
+        if len(np.ravel(s.shop_vector)) != 2400:
+            shop_data_list[i].shop_vector = np.concatenate([s.shop_vector,np.zeros((10-len(s.comment_word),200))])
+    user_data = file_load("../csv_data/user/user_lunch_data.csv")
+    data = ShopGraphDataset(user_data=user_data,shop_data_list=shop_data_list,date="昼")
+    dataset = data.load_dataset()
+    print(dataset.edge_index)
+    split_data = train_test_split_edges(dataset)
+    print(split_data)
+    # print(dataset.num_node_features)
+    # print(dataset.contains_isolated_nodes())
+    # print(dataset["x"].size())
+    # print(dataset["edge_index"].size())
+    # torch.set_printoptions(edgeitems=4000)
+    # for x in dataset["edge_index"]:
+    #     print(x)
---
+++ app/system/search.py
@@ -0,0 +1,30 @@
+from gensim.models import word2vec
+import numpy as np
+from nlptoolsjp.file_system import *
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# 検索機能
+def search_shop(keyword,shop_data_list):
+    model = word2vec.Word2Vec.load("data/model/word2vec/tablog_Skip-gram5.model")
+    select_data = []
+    max_kind = 0
+    target_shop = None
+    w_list = [w[0] for w in model.wv.most_similar(keyword,topn=9)] + [keyword]
+    for s in shop_data_list:
+        if set(s.shop_data["word"]+s.shop_data["ジャンル"]) & set(w_list) != set():
+            if max_kind < len(set(s.shop_data["word"]+s.shop_data["ジャンル"]) & set(w_list)):
+                target_shop = s
+                max_kind = len(set(s.shop_data["word"]+s.shop_data["ジャンル"]) & set(w_list))
+    for s in shop_data_list:
+        rate = cosine_similarity([np.ravel(target_shop.shop_vector[:2]),np.ravel(s.shop_vector[:2])])[0][0]
+        rate +=  mixed_similar(target_shop.shop_vector[2:],s.shop_vector[2:])
+        select_data.append((rate,s))
+    rate_data = np.argsort(-np.array([rate[0] for rate in select_data]))
+    return [select_data[index][1] for index in rate_data[:100]]
+
+def mixed_similar(array1,array2):
+    rate = 0
+    for i in range(len(array1)):
+        rate += max(cosine_similarity([array1[i]],array2)[0])
+    return rate/len(array1)
