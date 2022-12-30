f8f560db4762922c74c1e4164d77662267aed140
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-30 16:56:19 +0900

    オブジェクト群生成

--- shop_obj_create.py
+++ shop_obj_create.py
@@ -5,16 +5,19 @@ from nlptoolsjp.file_system import *
 data = {}
 cloased_data = file_load("csv_data/closed_shop.csv")
 word2vec_path = "word2vec/word2vec_model/tablog_Skip-gram5.model"
-for file in glob("tablog_data/*.json"):
-    print(file)
+for f,file in enumerate(glob("tablog_data/*.json")):
     if file_load(file)["口コミ"] == []:
         continue
     shop = Shop(shop_file_path=file)
     if shop.comment == [] or shop.shop_data["url"] in cloased_data["url"].tolist():
         continue
     shop.shop_text_clean()   
-    shop.word_vector_create(wd2vc_file_path=word2vec_path)
-    shop.id_select()
-    shop.vector_concat()
-    data[shop.shop_data["店名"]] = shop
-file_create(data,"shop_data.json")
+    try:
+        shop.word_vector_create(wd2vc_file_path=word2vec_path)
+        shop.id_select()
+        shop.vector_concat()
+    except:
+        print(file)
+        continue
+    data[shop.shop_data["店名"]] = shop.shop_data
+file_create(data,"shop_data_v2.json")
