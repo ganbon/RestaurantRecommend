1145a48a639ceeac7a4455dd2bab539dd23c9b39
ganbon <s20t301@stu.kagawa-u.ac.jp>
2022-12-31 20:32:55 +0900

    エラー修正

--- app/global_data.py
+++ app/global_data.py
@@ -2,5 +2,5 @@ from system.shop import Shop
 from nlptoolsjp.file_system import *
 
 
-shop_data = file_load("data/shop_data.json")
-SHOP_DATA = [Shop(shop_data=shop) for shop in shop_data]
+shop_data = file_load("data/shop_data_v2.json")
+SHOP_DATA = [Shop(shop_data=shop) for name,shop in shop_data.items()]
