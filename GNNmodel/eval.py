b986260d26b691ed534736cf0887ea538de992d6
ganbon <s20t301@stu.kagawa-u.ac.jp>
2023-01-23 01:33:36 +0900

    不要なファイル削除

--- GNNmodel/eval.py
+++ GNNmodel/eval.py
@@ -1,19 +1,20 @@
 from dataset import ShopGraphDataset
 import torch
-import pandas as pd
+from gensim.models import word2vec
 from nlptoolsjp.file_system import *
-import sys
-sys.path.append("..")
 from shop import Shop
+import time
 
 
+start = time.time()
 target = file_load("test.csv")
 target_shop = target["店名"].tolist()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 user_lunch_data = target[target["昼"]!=0]
-shop_data = file_load("../shop_data_v2.json")
-shop_data_list = [Shop(shop_data=shop) for name,shop in shop_data.items()]
-dataset_module = ShopGraphDataset(user_data = user_lunch_data,shop_data_list = shop_data_list,date="昼")
+wd2vec_model= word2vec.Word2Vec.load("../word2vec/word2vec_model/food_Skip-gram100-5.model")
+shop_data = file_load("../shop_data_v4.json")
+shop_data_list = [Shop(shop_data=shop,wd2vc_model=wd2vec_model) for name,shop in shop_data.items()]
+dataset_module = ShopGraphDataset(user_data = user_lunch_data,shop_data_list = shop_data_list,date="昼",vector_size=1200)
 shop_name = dataset_module.shop_name
 user_len = len(dataset_module.user_name)
 dataset = dataset_module.load_dataset()
@@ -23,7 +24,7 @@ edge_index = dataset.edge_index.long().to(device)
 print(edge_index)
 edge_attr = dataset.edge_attr.double().to(device)
 print(edge_attr)
-model = torch.load('model/lunch_vgae.pth').to(device)
+model = torch.load('model/lunch_vgae_v2.pth').to(device)
 z = model.encode(node_feature,edge_index,edge_attr)
 result = model.decoder.forward_all(z)
 # print(result.size())
@@ -43,3 +44,4 @@ for i,r in enumerate(rank):
     print(shop_name[r-user_len])
     print(result[0][r])
     print(shop_data[shop_name[r-user_len]]["url"])
+print(time.time()-start)
